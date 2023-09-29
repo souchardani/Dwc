ee303cbb891d68fb4c06700fc169c7cb09da12fb
souchardani <114416167+souchardani@users.noreply.github.com>
2023-09-29 23:06:17 +0200

    FINALIZADO REGREOS AL FUTURO

--- regresoAlFuturoDanielSouchar/app.js
+++ regresoAlFuturoDanielSouchar/app.js
@@ -1,126 +1,101 @@
-let fechaPresente = null;
-let fechaDestino = null;
+//calculamos la diferencia de tiempoo
+function diferenciaTiempo() {
+  // Obtenemos los valores de los campos de entrada
+  const presenteMes = document.getElementById("presenteMes").value;
+  const presenteDia = parseInt(document.getElementById("presenteDia").value);
+  const presenteAno = parseInt(document.getElementById("presenteAno").value);
+  const presenteHora = parseInt(document.getElementById("presenteHora").value);
+  const presenteMinuto = parseInt(
+    document.getElementById("presenteMinuto").value
+  );
 
-function CalcularAno() {
-  presenteAno = fechaPresente.getFullYear();
-  viajeAno = fechaDestino.getFullYear();
-  console.log(presenteAno);
-  console.log(viajeAno);
+  const destinoMes = document.getElementById("destinoMes").value;
+  const destinoDia = parseInt(document.getElementById("destinoDia").value);
+  const destinoAno = parseInt(document.getElementById("destinoAno").value);
+  const destinoHora = parseInt(document.getElementById("destinoHora").value);
+  const destinoMinuto = parseInt(
+    document.getElementById("destinoMinuto").value
+  );
 
-  console.log(`presente año ${presenteAno}`);
-  if (presenteAno > destinoAno) {
-    intervalAno = presenteAno - destinoAno;
-    return "pasado";
-  } else if (destinoAno > presenteAno) {
-    intervalAno = destinoAno - presenteAno;
-    return "futuro";
-  } else {
-    intervalAno = 0;
-    return "igual";
-  }
-}
-
-function CalcularDia() {
-  if (presenteDia > destinoDia) {
-    intervalDia = presenteDia - destinoDia;
-    return "pasado";
-  } else if (destinoDia > presenteDia) {
-    intervalDia = destinoDia - presenteDia;
-    return "futuro";
-  } else {
-    intervalDia = 0;
-    return "igual";
-  }
-}
-
-function calcularMensaje() {
-  let tipoAno = CalcularAno();
-  console.log("intervalo años: " + intervalAno);
-  console.log("tipo año: " + tipoAno);
-  let tipoDia = CalcularDia();
-  console.log("intervalo dias: " + intervalDia);
-  console.log(`Futuro dia: ${tipoDia}`);
-  if (tipoAno == "futuro") {
-    mensaje = `Has viajado al futuro: ${intervalAno} Años, ${intervalDia} dias,`;
-  } else if (tipoAno == "pasado") {
-    mensaje = `Has viajado al pasado: ${intervalAno} Años, ${intervalDia} dias,`;
-  } else {
-    //los años son iguales
-    if (tipoDia == "futuro") {
-      mensaje = `Has viajado al futuro: ${intervalAno} Años, ${intervalDia} dias,`;
-    } else if (tipoDia == "pasado") {
-      mensaje = `Has viajado al pasado: ${intervalAno} Años, ${intervalDia} dias,`;
-    } else {
-      //los dias son iguales
-      console.log("hola");
-    }
-  }
-  return mensaje;
-}
-
-function MostrarMensaje() {
-  calcularFechas();
-  let mensaje = null;
-  let intervalAno = null;
-  let MostrarMensaje = document.getElementById("mensaje");
-  mensaje = calcularMensaje();
-  MostrarMensaje.innerHTML = mensaje;
-}
-
-document.getElementById("calcular").addEventListener("click", MostrarMensaje);
-
-function calcularFechas() {
-  let presenteAno = document.getElementById("presenteAno").value;
-  let destinoAno = document.getElementById("destinoAno").value;
-  let presenteDia = document.getElementById("presenteDia").value;
-  let destinoDia = document.getElementById("destinoDia").value;
-  let presenteMes = document.getElementById("presenteMes").value;
-  let destinoMes = document.getElementById("destinoMes").value;
-  let presenteHora = document.getElementById("presenteHora").value;
-  let presenteMinuto = document.getElementById("presenteMinuto").value;
-  let destinoHora = document.getElementById("destinoHora").value;
-  let destinoMinuto = document.getElementById("destinoMinuto").value;
-
-  presenteMes = convertirMes(presenteMes);
-  destinoMes = convertirMes(destinoMes);
-  console.log(presenteMes);
-  console.log(destinoMes);
-
-  fechaPresente = new Date(
+  //calculamos fechas
+  const fechaPresente = new Date(
     presenteAno,
-    presenteMes,
+    obtenerNumeroMes(presenteMes),
     presenteDia,
     presenteHora,
     presenteMinuto
   );
-  fechaDestino = new Date(
+  const fechaDestino = new Date(
     destinoAno,
-    destinoMes,
+    obtenerNumeroMes(destinoMes),
     destinoDia,
     destinoHora,
     destinoMinuto
   );
 
-  console.log(fechaPresente);
-  console.log(fechaDestino);
+  //diferencia de tiempo en milisegundos
+  const diferenciaTiempo = fechaDestino - fechaPresente;
+
+  if (diferenciaTiempo === 0) {
+    mostrarResultado("No te moverás en el tiempo.");
+  } else if (diferenciaTiempo < 0) {
+    mostrarResultado(
+      `Viajarás al pasado ${calcularTiempoPasado(diferenciaTiempo)}.`
+    );
+  } else {
+    mostrarResultado(
+      `Viajarás al futuro ${calcularTiempoFuturo(diferenciaTiempo)}.`
+    );
+  }
 }
 
-function convertirMes(presenteMes) {
-  let meses = [
-    "ENE",
-    "FEB",
-    "MAR",
-    "ABR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AGO",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DIC",
+// Función para obtener el número del mes
+function obtenerNumeroMes(mes) {
+  const meses = [
+    "ene",
+    "feb",
+    "mar",
+    "abr",
+    "may",
+    "jun",
+    "jul",
+    "ago",
+    "sep",
+    "oct",
+    "nov",
+    "dic",
   ];
-
-  indice = meses.indexOf(presenteMes);
-  return indice;
+  return meses.indexOf(mes.toLowerCase());
 }
+
+// Función para calcular el tiempo en el pasado
+function calcularTiempoPasado(time) {
+  const segundos = Math.floor(Math.abs(time) / 1000);
+  const minutos = Math.floor(segundos / 60);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+  const anos = Math.floor(dias / 365);
+  return `${anos} años, ${dias % 365} días, ${horas % 24} horas y ${
+    minutos % 60
+  } minutos`;
+}
+
+// Función para calcular el tiempo en el futuro
+function calcularTiempoFuturo(time) {
+  const segundos = Math.floor(time / 1000);
+  const minutos = Math.floor(segundos / 60);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+  const anos = Math.floor(dias / 365);
+  return `${anos} años, ${dias % 365} días, ${horas % 24} horas y ${
+    minutos % 60
+  } minutos`;
+}
+
+function mostrarResultado(mensaje) {
+  const mensajeElement = document.getElementById("mensaje");
+  mensajeElement.textContent = mensaje;
+}
+
+//añadimos el event listener
+document.getElementById("calcular").addEventListener("click", diferenciaTiempo);
