c355556abb086c8b47547d90b2d2bd82c9f23360
souchardani <114416167+souchardani@users.noreply.github.com>
2023-11-07 22:16:14 +0100

    cambios

---
+++ ejerciciosjspractica/formulariosPractica/app.js
@@ -0,0 +1,80 @@
+let txtNombre;
+let txtApellido;
+let inputs;
+let inputMarcas;
+// let coches = [
+//   {
+//     marca: "Mercedes",
+//     modelos: ["mercedes1", "mercedes2", "mercedes3"],
+//   },
+//   {
+//     marca: "BMW",
+//     modelos: ["BMW1", "BMW2", "BMW3"],
+//   },
+//   {
+//     marca: "toyota",
+//     modelos: ["toyota1", "toyota2", "toyota3"],
+//   },
+
+// ];
+const coches = {
+  toyota: ["Corolla", "Camry", "Rav4"],
+  honda: ["Civic", "Accord", "CR-V"],
+  ford: ["Fiesta", "Focus", "Mustang"],
+};
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  txtNombre = document.getElementById("Nombre");
+  txtApellido = document.getElementById("Apellido");
+  inputs = document.querySelectorAll(
+    "input[type=text],textarea,select,input[type=radio],input[type=number], input[type=email]"
+  );
+  inputMarcas = document.getElementById("marca");
+  inputMarcas.addEventListener("change", cargarModelo);
+  colortxt();
+  cargarMarcas();
+}
+
+function colortxt() {
+  inputs.forEach((element) => {
+    element.addEventListener("focus", () => {
+      element.style.backgroundColor = "gray";
+    });
+  });
+  inputs.forEach((element) => {
+    element.addEventListener("blur", () => {
+      element.style.backgroundColor = "white";
+    });
+  });
+}
+
+function cargarMarcas() {
+  //   coches.forEach((element) => {
+  //     let option = document.createElement("option");
+  //     option.textContent = element.marca;
+  //     option.value = element.marca;
+  //     inputMarcas.appendChild(option);
+  //   });
+  Object.keys(coches).forEach((key) => {
+    let option = document.createElement("option");
+    option.textContent = key;
+    option.value = key;
+    inputMarcas.appendChild(option);
+  });
+}
+
+function cargarModelo() {
+  inputMarcas = document.getElementById("marca");
+  let valor = inputMarcas.value;
+  console.log(valor);
+  inputModelo = document.getElementById("modelo");
+  inputModelo.innerHTML = "";
+  coches[valor].forEach((element) => {
+    let option = document.createElement("option");
+    option.textContent = element;
+    option.value = element;
+    inputModelo.appendChild(option);
+  });
+}
